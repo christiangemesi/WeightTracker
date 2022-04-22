3730110483a1e54df1384443fcfbdc44fbf5e75d
Christians Desktop <christian.gemesi@students.fhnw.ch>
2022-04-22 16:32:46 +0200

    created some basic controllers and html templates

---
+++ demo/src/main/java/com/example/demo/Controllers/HomeScreenController.java
@@ -0,0 +1,18 @@
+package com.example.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeScreenController {
+
+    @RequestMapping("/")
+    public String  welcome() {
+        return "home.html";
+    }
+
+}
