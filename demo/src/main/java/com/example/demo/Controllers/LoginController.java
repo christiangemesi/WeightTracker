6d391565737e135a7b87471d4c14acf692d1d40b
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2022-04-29 15:44:55 +0200

    worked on signup

--- demo/src/main/java/com/example/demo/Controllers/LoginController.java
+++ demo/src/main/java/com/example/demo/Controllers/LoginController.java
@@ -1,16 +1,28 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Service.UserService;
+import com.example.demo.model.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Controller
 public class LoginController {
+
+    private final UserService userService;
+
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping(path = "/login", method = RequestMethod.GET)
     public String login(Authentication authentication, @RequestParam("error") Optional<String> error, Model model) {
@@ -22,4 +34,28 @@ public class LoginController {
             return "login";
         }
     }
+
+    @RequestMapping(path = "/signup", method = RequestMethod.GET)
+    public String register() {
+        return "signup";
+    }
+
+    @RequestMapping(path = "/signup", method = RequestMethod.POST)
+    public String register( User user, BindingResult bindingResult, Model model) {
+        if (this.userService.usernameAlreadyExists(user.getUsername())) {
+            bindingResult.addError(new FieldError("user", "username", "Username already exists"));
+        }
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("user", user);
+
+            return "signup";
+        } else {
+            this.userService.addUser(user.getUsername(), user.getPassword(), Set.of("ROLE_USER"));
+
+            return "redirect:/login";
+        }
+    }
+
+
 }
--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -17,13 +17,14 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-
     @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
     private String password;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> authorities;
     @OneToMany(mappedBy = "user")
     private Set<WeightEntry> weightEntrySet = new HashSet<>();
 
@@ -46,9 +47,6 @@ public class User implements UserDetails {
         this.authorities = authorities;
         this.weightEntrySet = weightEntrySet;
     }
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    private Set<String> authorities;
 
     @Override
     public String toString() {
