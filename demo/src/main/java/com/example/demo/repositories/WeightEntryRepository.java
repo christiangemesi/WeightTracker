81dfc64908fbca1496655f705bd717b99585133d
Christians Desktop <christian.gemesi@students.fhnw.ch>
2022-05-02 15:33:28 +0200

    user now is logged out when going to signup page

--- demo/src/main/java/com/example/demo/repositories/WeightEntryRepository.java
+++ demo/src/main/java/com/example/demo/repositories/WeightEntryRepository.java
@@ -6,4 +6,20 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WeightEntryRepository extends JpaRepository<WeightEntry,Long> {
+
+
+    /*
+    @Query(
+        "SELECT report"
+            + " FROM "
+            + "Report report"
+            + " WHERE "
+            + "report.incident.id = :#{#path.incidentId}"
+            + " AND "
+            + "report.id = :id"
+    )
+    @Override
+    Optional<Report> findByPath(@Param("path") ReportPath path, @Param("id") Long id);
+     */
+
 }
