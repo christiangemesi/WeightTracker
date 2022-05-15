77b7c07c312b9cc8c8a6d91180b8eeb7c5397186
Daniel von Atzigen <daniel@vatz.ch>
2022-05-15 22:27:32 +0200

    Add `AccountService` for auth session handling

---
+++ demo/src/main/java/com/example/demo/services/AccountService.java
@@ -0,0 +1,40 @@
+package com.example.demo.services;
+
+import com.example.demo.models.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AccountService {
+    public void setCurrentUser(User user) {
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+            user,
+            user.getPassword(),
+            user.getAuthorities()
+        );
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    public void clearCurrentUser() {
+        SecurityContextHolder.getContext().setAuthentication(null);
+    }
+
+    public Optional<User> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            return Optional.empty();
+        }
+        User currentUser = (User) authentication.getPrincipal();
+        return Optional.of(currentUser);
+    }
+
+    public User requireCurrentUser() {
+        return getCurrentUser().orElseThrow(() -> (
+            new IllegalStateException("not authenticated")
+        ));
+    }
+}
